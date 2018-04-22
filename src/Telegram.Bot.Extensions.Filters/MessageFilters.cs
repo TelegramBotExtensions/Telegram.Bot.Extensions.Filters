67733a7319f799520d19b501f1b08b82f5fe836e
Arne Kiesewetter <banane1996@gmx.net>
2018-04-22 23:07:37 +0200

    Add targeted @name option to Command method

--- src/Telegram.Bot.Extensions.Filters/MessageFilters.cs
+++ src/Telegram.Bot.Extensions.Filters/MessageFilters.cs
@@ -76,26 +76,31 @@ namespace Telegram.Bot.Extensions.Filters
         /// Creates a <see cref="Filter{T}"/> that matches any command
         /// at the start of a message, with the specified number of parameters.
         /// </summary>
+        /// <param name="name">The name of the bot.</param>
+        /// <param name="mustHaveName">Whether the command must be of the form <c>command@name</c>.</param>
         /// <param name="minParameters">The minimum number of parameters it needs to have.</param>
         /// <param name="maxParameters">The maximum number of parameters it can have. Keep at -1 for unlimited.</param>
         /// <param name="checkText">Whether to check the <see cref="Message.Text"/>.</param>
         /// <param name="checkCaption">Whether to check the <see cref="Message.Caption"/>.</param>
         /// <returns>A <see cref="Filter{T}"/> that matches any command
         /// at the start of a message, with the specified number of parameters.</returns>
-        public static Filter<Message> Command(int minParameters = 0, int maxParameters = -1, bool checkText = true, bool checkCaption = true)
-            => Regex($"^/\\S+(\\s\\S+){{{minParameters},{(maxParameters < 0 ? "" : maxParameters.ToString())}}}$", checkText, checkCaption);
+        public static Filter<Message> Command(string name = "", bool mustHaveName = false, int minParameters = 0, int maxParameters = -1, bool checkText = true, bool checkCaption = true)
+            => Command($"\\S+", name, mustHaveName, minParameters, maxParameters, checkText, checkCaption);
 
         /// <summary>
         /// Creates a <see cref="Filter{T}"/> that matches the given command
         /// at the start of a message, with the specified number of parameters.
         /// </summary>
+        /// <param name="name">The name of the bot.</param>
+        /// <param name="mustHaveName">Whether the command must be of the form <c>command@name</c>.</param>
+        /// <param name="command">The command (treated as regex) to match.</param>
         /// <param name="minParameters">The minimum number of parameters it needs to have.</param>
         /// <param name="maxParameters">The maximum number of parameters it can have. Keep at -1 for unlimited.</param>
         /// <param name="checkText">Whether to check the <see cref="Message.Text"/>.</param>
         /// <param name="checkCaption">Whether to check the <see cref="Message.Caption"/>.</param>
         /// <returns>A <see cref="Filter{T}"/> that matches the given command
         /// at the start of a message, with the specified number of parameters.</returns>
-        public static Filter<Message> Command(string command, int minParameters = 0, int maxParameters = -1, bool checkText = true, bool checkCaption = true)
-            => Regex($"^/{command}(\\s\\S+){{{minParameters},{(maxParameters < 0 ? "" : maxParameters.ToString())}}}$", checkText, checkCaption);
+        public static Filter<Message> Command(string command, string name = "", bool mustHaveName = false, int minParameters = 0, int maxParameters = -1, bool checkText = true, bool checkCaption = true)
+            => Regex($"^/{command}(?:{(name.StartsWith("@") ? name : $"@{name}")}){(mustHaveName ? "{1}" : "?")}(?:\\s+\\S+){{{minParameters},{(maxParameters < 0 ? "" : maxParameters.ToString())}}}\\s*$", checkText, checkCaption);
     }
 }
